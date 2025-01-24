a574ff9a3f774061a1e10d61e0f891dde3e3a356
Angel Uriel Elizalde Lopez <angelurielelizaldelopezids@gmai.com>
2025-01-24 00:04:18 -0600

    Rediseñando

---
+++ js/script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // Selecciona todas las barras de progreso
+    const progressBars = document.querySelectorAll(".progress");
+  
+    progressBars.forEach((bar) => {
+      // Obtiene el valor del porcentaje desde data-percentage
+      const percentage = bar.getAttribute("data-percentage");
+  
+      // Asigna el ancho dinámicamente
+      bar.style.width = percentage + "%";
+    });
+  });
+  
