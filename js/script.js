955c4690fb716a2eecd43324578dd5ce3da4bd79
Angel Uriel Elizalde Lopez <angelurielelizaldelopezids@gmai.com>
2025-01-24 09:42:58 -0600

    agregando cv

--- js/script.js
+++ js/script.js
@@ -10,4 +10,12 @@ document.addEventListener("DOMContentLoaded", () => {
       bar.style.width = percentage + "%";
     });
   });
-  
+
+
+
+  function descargarArchivo() {
+    const link = document.createElement("a"); // Crear un elemento <a>
+    link.href = "img/perfil.png"; // Ruta del archivo (puede ser PDF o JPG)
+    link.download = "Mi_CV.pdf"; // Nombre del archivo al descargar
+    link.click(); // Simula el clic en el enlace
+  }
